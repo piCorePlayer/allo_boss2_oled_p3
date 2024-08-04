967d9a0b04729319cc83784519ebda5414e4f169
paul-1 <6473457+paul-1@users.noreply.github.com>
2024-08-04 12:00:23 -0400

    Update for python3.11 pCP 14.x

--- create_tcz.sh
+++ create_tcz.sh
@@ -4,12 +4,12 @@ EXTNAM="allo-boss2"
 TMPDIR="/tmp/$EXTNAM"
 
 mkdir -p $TMPDIR/usr/local/bin
-mkdir -p $TMPDIR/usr/local/lib/python3.8/site-packages
+mkdir -p $TMPDIR/usr/local/lib/python3.11/site-packages
 mkdir -p $TMPDIR/usr/local/share/allo-boss2
 
 cd boss2_oled_p3
 
-cp -avr Boss2_Hardware $TMPDIR/usr/local/lib/python3.8/site-packages/
+cp -avr Boss2_Hardware $TMPDIR/usr/local/lib/python3.11/site-packages/
 cp boss2_oled* $TMPDIR/usr/local/bin
 cp pcp.bmp $TMPDIR/usr/local/share/allo-boss2
 cp README $TMPDIR/usr/local/share/allo-boss2
